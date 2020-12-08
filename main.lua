3e17e90605716b92b5037b5c828987a74f79e03f
sTaissa <61012521+sTaissa@users.noreply.github.com>
2020-12-08 11:41:19 -0300

    pong0

--- main.lua
+++ main.lua
@@ -1,3 +1,14 @@
+WINDOW_WIDTH = 1280 --captalize indicates constants
+WINDOW_HEIGHT = 720
+
+function love.load()
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false, --not fullscreen
+        vsync = true, --sincronized with monitor
+        resizable = false --can't change the window size
+    }) --set the size of the window
+end
+
 function love.draw()
-    love.graphics.print("Hello World!")
+    love.graphics.printf("Hello World!", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
 end
